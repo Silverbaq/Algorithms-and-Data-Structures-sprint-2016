4e9e1a558fccab12c4684061f4bd9da413432724
malikbekkouche <malik-bekkouche@outlook.dk>
2016-03-30 11:25:06 +0200

    deleted bs comments

--- RunSort.java
+++ RunSort.java
@@ -4,7 +4,7 @@ public class RunSort{
 static Comparable[] aux;
 public static void sort(Comparable[] a)
 {
-aux = new Comparable[a.length]; // Allocate space just once.
+aux = new Comparable[a.length];
 boolean b=true;
 int low=0,mid=0,high=0;
 while(!isSorted(a)){
@@ -13,82 +13,52 @@ while(i<a.length){
 	if(b)
 		low=i;
 	int j=i+1;
-	//String str=(String)a[i];
-	//int lengthOfRun=0;
 	while(j<a.length&&(less(a[i],a[j])||a[i].equals(a[j]))){
-//		str+=a[j];
 		i=j;
-		j++;
-		//lengthOfRun++;
-}
-	//if(lengthOfRun<8){
-		//insertionSort(a,j);
-	//}
-	
+		j++;}
 	if(b)
 		mid=j-1;
-	if(!b){
+	else{
 		high=j-1;
-		if(high < mid){ j=a.length;}
+		if(high < mid){ j=a.length;} // if there is an odd number of runs do not merge the last one
 		else{
-		StdOut.println(low+" "+mid+" "+high);
 		merge(a,low,mid,high);}}
 	i=j;
 	b=!b;
-		}	
+		}
 	}
 }
-/*public static void insertionSort(Comparable[] a, int k){
-int N =0;
-if(k+8<a.length)
-N=k+8;
-else
-N=a.length;
-for (int i = 1; i < N; i++)
-{ // Insert a[i] among a[i-1], a[i-2], a[i-3]... ..
-for (int j = i; j > 0 && less(a[j], a[j-1]); j--)
-exch(a, j, j-1);
-}
-}
-private static void exch(Comparable[] a, int i, int j)
-{ Comparable t = a[i]; a[i] = a[j]; a[j] = t; }*/
-public static void merge(Comparable[] a, int lo, int mid, int hi)
-{ // Merge a[lo..mid] with a[mid+1..hi].
+
+public static void merge(Comparable[] a, int lo, int mid, int hi){ // merge from the book Algorithms, 4th ed.
 int i = lo, j = mid+1;
-for (int k = lo; k <= hi; k++) // Copy a[lo..hi] to aux[lo..hi].
+for (int k = lo; k <= hi; k++)
 aux[k] = a[k];
-for (int k = lo; k <= hi; k++) // Merge back to a[lo..hi].
+for (int k = lo; k <= hi; k++)
 if (i > mid) a[k] = aux[j++];
 else if (j > hi ) a[k] = aux[i++];
 else if (less(aux[j], aux[i])) a[k] = aux[j++];
 else a[k] = aux[i++];
-for (int k = lo; k <= hi; k++) //printing
-	StdOut.print(a[k]);
-StdOut.println("");
-}	
-private static boolean less(Comparable v, Comparable w) // orEqual !!!!!! or non redondant input
+
+}
+private static boolean less(Comparable v, Comparable w)
 { return v.compareTo(w) <0; }
 public static boolean isSorted(Comparable[] a){
-    boolean sorted = true;        
+    boolean sorted = true;
     for (int i = 1; i < a.length; i++) {
         if (less(a[i],a[i-1])) sorted = false;}
     return sorted;
 }
 public static void main(String[] args){
-//String[] a={"a","a","b","t","d","j","g","a","x","y","k"};
-//{"b","x","a","d","f","k","b","a","g","c","j","u","y","z"};
-ArrayList<String> list=new ArrayList<String>();
-
+ArrayList<Comparable> list=new ArrayList<Comparable>();
 while(!StdIn.isEmpty()){
-String st=StdIn.readString();
+Comparable st=(Comparable)StdIn.readString();
 list.add(st);
 }
-
-String[] a=new String[list.size()];
+Comparable[] a=new Comparable[list.size()];
 for(int i=0;i<a.length;i++)
 	a[i]=list.get(i);
 sort(a);
 for(int i=0;i<a.length;i++)
 	StdOut.println(a[i]);
-	}
+}
 }
--- RunSort2.java
+++ RunSort2.java
@@ -2,52 +2,42 @@ import edu.princeton.cs.algs4.*;
 import java.util.*;
 public class RunSort2{
 static Comparable[] aux;
-public static void sort(Comparable[] a)
-{
-aux = new Comparable[a.length]; // Allocate space just once.
+public static void sort(Comparable[] a){
+aux = new Comparable[a.length];
 boolean b=true;
-int low=0,mid=0,high=0,op=0;
+int low=0,mid=0,high=0;
 while(!isSorted(a)){
-int i=0;op++;
+int i=0;
 while(i<a.length){
 	if(b)
 		low=i;
 	int j=i+1;
-	//String str=(String)a[i];
-	int lengthOfRun=1;
-	//int t=i;
+	int lengthOfRun=1;//length of the current run
 	while(j<a.length&&(less(a[i],a[j])||a[i].equals(a[j]))){
-//		str+=a[j];
 		i=j;
 		j++;
-		lengthOfRun++;
-}
-	if(lengthOfRun<8){
-		insertionSort(a,j);
-		//StdOut.println(op+"j = "+j);
-	}
-
+		lengthOfRun++;}
+	if(lengthOfRun<8){// use insertion sort for runs less than 8 elements
+		insertionSort(a,j);}
 	if(b)
 		mid=j-1;
 	if(!b){
 		high=j-1;
 		if(high < mid){ j=a.length;}
 		else{
-
 		merge(a,low,mid,high);}}
 	i=j;
 	b=!b;
 		}
 	}
 }
-public static void insertionSort(Comparable[] a, int k){
+public static void insertionSort(Comparable[] a, int k){//from the book Algorithms, 4th ed. (with small changes in the array indexes)
 int N =0;
 if(k+8<a.length)
 N=k+8;
 else
 N=a.length;
-for (int i = k+1; i < N; i++)
-{ // Insert a[i] among a[i-1], a[i-2], a[i-3]... ..
+for (int i = k+1; i < N; i++){
 for (int j = i; j > k && less(a[j], a[j-1]); j--)
 exch(a, j, j-1);
 }
@@ -55,12 +45,11 @@ exch(a, j, j-1);
 }
 private static void exch(Comparable[] a, int i, int j)
 { Comparable t = a[i]; a[i] = a[j]; a[j] = t; }
-public static void merge(Comparable[] a, int lo, int mid, int hi)
-{ // Merge a[lo..mid] with a[mid+1..hi].
+public static void merge(Comparable[] a, int lo, int mid, int hi){
 int i = lo, j = mid+1;
-for (int k = lo; k <= hi; k++) // Copy a[lo..hi] to aux[lo..hi].
+for (int k = lo; k <= hi; k++)
 aux[k] = a[k];
-for (int k = lo; k <= hi; k++) // Merge back to a[lo..hi].
+for (int k = lo; k <= hi; k++)
 if (i > mid) a[k] = aux[j++];
 else if (j > hi ) a[k] = aux[i++];
 else if (less(aux[j], aux[i])) a[k] = aux[j++];
@@ -76,15 +65,11 @@ public static boolean isSorted(Comparable[] a){
     return sorted;
 }
 public static void main(String[] args){
-//int[] a={111,10111,121658,1533893,1538464,56498};
-//Comparable []a={"b","x","a","d","f","k","b","a","g","c","j","u","y","z"};
 ArrayList<Comparable> list=new ArrayList<Comparable>();
-
 while(!StdIn.isEmpty()){
 Comparable st=(Comparable)StdIn.readString();
 list.add(st);
 }
-
 Comparable[] a=new Comparable[list.size()];
 for(int i=0;i<a.length;i++)
 	a[i]=list.get(i);
