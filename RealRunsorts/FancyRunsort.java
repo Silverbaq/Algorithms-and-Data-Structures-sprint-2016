5bc9de8f84ee794de1ade5f2c401a1506bec4a63
malikbekkouche <malik-bekkouche@outlook.dk>
2016-04-16 12:44:21 +0200

    i hope it works now

--- RealRunsorts/FancyRunsort.java
+++ RealRunsorts/FancyRunsort.java
@@ -4,10 +4,11 @@ public class FancyRunsort{
 static Comparable[] aux;
 public static void sort(Comparable[] a){
 aux = new Comparable[a.length];
-boolean b=true;
+boolean b=true;boolean didSort=false;
 int low=0,mid=0,high=0;
 while(!isSorted(a)){
 	int i=0;
+	didSort=false;
 	while(i<a.length){
 		if(b)
 			low=i;
@@ -17,27 +18,38 @@ while(!isSorted(a)){
 			i=j;
 			j++;
 			lengthOfRun++;}
-		if(lengthOfRun<8)
-			insertionSort(a,8-lengthOfRun,j);
-		if(b)
-			mid=j-1;
-		else{
-			high=j-1;
-			if(high < mid) 
-				j=a.length; // if there is an odd number of runs do not merge the last one
-			else
-				merge(a,low,mid,high);
-			}
+		if(lengthOfRun<8){
+			insertionSort(a,j-lengthOfRun);
+			didSort=true;
+		}
+		if(b) 
+			mid=limit(didSort,a,j-1,lengthOfRun);
+			else {
+				high=limit(didSort,a,j-1,lengthOfRun);
+				if(high < mid) 
+					j=a.length; // if there is an odd number of runs do not merge the last one
+				else
+					merge(a,low,mid,high);
+				}
 		i=j;
 		b=!b;
 	}
 }
 }
+public static int limit(boolean b,Comparable[] a,int j,int lor){
+if(b)
+	if(j-lor+8<a.length)
+		return j-lor+8;
+	else
+		return a.length-1;
+else	
+	return j;
+}
 
-public static void insertionSort(Comparable[] a, int k,int s){//from the book Algorithms, 4th ed. (with small changes in the array indexes)
-int N =0;													  // k is from where to begin the sort // s is the number of elements to sort
-if(k+s<a.length)
-N=k+s;
+public static void insertionSort(Comparable[] a, int k){//from the book Algorithms, 4th ed. (with small changes in the array indexes)
+int N =0;													  // k is the begining index 
+if(k+8<a.length)
+N=k+8;
 else
 N=a.length;
 for (int i = k+1; i < N; i++){
